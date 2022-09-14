a7aa0a760672fea9e97cd5c375f8191dcc19b828
Carolina <ci.arayagonzalez@gmail.com>
2022-09-14 17:56:13 -0300

    feat: create -- create component BulletGroup

--- react/components/BulletGroup/index.tsx
+++ react/components/BulletGroup/index.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
+import { BulletsSchema } from "./BulletTypes";
+import { useDevice } from "vtex.device-detector"
 
-const BulletGroup = () => {
-    return (
-        <div>Aqui tendremos un list contex</div>
-    )
+export interface BulletGroupProps {
+    bullets: BulletsSchema
+}
+
+const BulletGroup = ({
+    bullets,
+    children
+}: PropsWithChildren<BulletGroupProps>) => {
+    const { isMobile } = useDevice();
+    console.log(bullets)
+    if (false) {
+        children
+    }
+    return ( isMobile ? <div>Estamos en Mobile</div> : <div>Estamos en Desktop</div>)
 } 
 
 export default BulletGroup;
